136e62d4d78d6d6cb763f44063e82c73e59624bf
Joey Steigelman <joey.steigelman@gmail.com>
2022-01-14 13:37:34 -0500

    set up testing with authentication

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'joey.steigelman@gmail.com',
+        from: process.env.MY_EMAIL,
         subject: 'Welcome to the app!',
         text: `Welcome, ${name}! Hope you enjoy the app.`
     })
@@ -14,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancellationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'joey.steigelman@gmail.com',
+        from: process.env.MY_EMAIL,
         subject: 'Cancellation confirmation',
         text: `Hello, ${name}! This is a confirmation that you have successfully unsubscribed from our mailing list.`
     })
---
+++ tests/user.test.js
@@ -0,0 +1,107 @@
+const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+
+const userOne = {
+  _id: userOneId,
+  name: 'Jel',
+  email: 'joey.steigelman@gmail.com',
+  password: 'BlUEeeE1!',
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
+}
+
+beforeEach(async () => {
+  await User.deleteMany()
+  await new User(userOne).save()
+})
+
+test('Should signup a new user', async () => {
+  const response = await request(app)
+    .post('/users')
+    .send({
+      name: 'Joey',
+      email: 'joey.steigelman.2@gmail.com',
+      password: 'BLuEe!!!',
+    })
+    .expect(201)
+
+    // Assert that the database was changed correctly
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // Assertions about the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Joey',
+            email: 'joey.steigelman.2@gmail.com',
+        },
+        token: user.tokens[0].token
+    })
+    expect(user.password).not.toBe('BLuEe!!!')
+})
+
+test('Should login existing user', async () => {
+
+    const response = await request(app)
+        .post('/users/login')
+        .send({
+        email: userOne.email,
+        password: userOne.password,
+        })
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(response.body.token).toBe(user.tokens[1].token)
+
+})
+
+test('Should not login nonexistent user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: 'thiswillfail',
+    })
+    .expect(400)
+})
+
+test('Should get profile for user', async () => {
+  await request(app)
+    .get('/users/me')
+    .set('Authorization', userOne.tokens[0].token)
+    .send()
+    .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('Should delete account for user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', userOne.tokens[0].token)
+        .send()
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
+})
+
+test('Should not delete account for unauthenticated user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
+})
